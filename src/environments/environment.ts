0cad9e2f9e70c6b4416aaaac4054da3a712e3add
taiga.kuromatsu <taiga.kuromatsu@pathoslogos.co.jp>
2025-10-23 16:43:47 +0900

    appcheck

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,21 @@
+// src/environments/environment.ts
 export const environment = {
-    production: false,
-    firebase: {
-      apiKey: "AIzaSyDgfe0dX1VPD9ggmYc2xbk5OjY3ZmjzVWQ",
-      authDomain: "kensyu10121.firebaseapp.com",
-      projectId: "kensyu10121",
-      storageBucket: "kensyu10121.firebasestorage.app",
-      messagingSenderId: "210275340301",
-      appId: "1:210275340301:web:6f6d12b2c000bd883a8544",
-      measurementId: "G-DFR7RTB005"
-    }
-  };
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDgfe0dX1VPD9ggmYc2xbk5OjY3ZmjzVWQ",
+    authDomain: "kensyu10121.firebaseapp.com",
+    projectId: "kensyu10121",
+    //  バケット名に修正（ドメインではなく）
+    storageBucket: "kensyu10121.appspot.com",
+    messagingSenderId: "210275340301",
+    appId: "1:210275340301:web:6f6d12b2c000bd883a8544",
+    measurementId: "G-DFR7RTB005"
+  },
+  // App Check（reCAPTCHA Enterprise）のサイトキー
+  appCheck: {
+    siteKey: "6LfbAfQrAAAAAC3vk2WxyL2FTZTQ5mkLLZErGEfv",
+    debugToken: 'BB6EB0CC-9784-4B6B-B11C-82FED1FDCDA8'
+  }
+};
+
   
